13e7f0bc46e8404b166cb947a0bd332d5cfa4afa
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2022-02-08 22:08:00 -0500

    8th Feb 2022 - Tester

---
+++ Tester3.java
@@ -0,0 +1,9 @@
+//tester
+public class Tester3 {
+	public static void main (String []args) {
+Scholar scholar = new Scholar("Tom", 2000 ,"Dentist"); 
+Scholar scholar1 = new Scholar("Lisa", 1990, "Lawyer");
+Instructor instructor1 = new Instructor ("Jerry", 1987, "Teacher", 4500);
+Instructor instructor2 = new Instructor ("Helen", 1977, "Scientist", 6000);
+}
+}
